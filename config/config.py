863235c62bd78c985662c86bd1044ad473231754
kien3007 <kiennguyen300703@gmail.com>
2024-12-07 12:45:33 +0700

    finalproject

---
+++ config/config.py
@@ -0,0 +1,18 @@
+config = {
+    "openai": {
+        "api_key": "sk-yIHKRujBlWoCPVCP1NGmF9Sr1Dy9mZeJDqCTJvGl7QODjI2Y"
+    },
+    "kafka": {
+        "sasl.username": "ZGHTEORGDTSYF7F2",
+        "sasl.password": "e/CPRtaTaHPfEyjDr0qEKQbSYc6g+cecNCdL+pPfYVNxm8PtcTp5lxMhjRmheYCe",
+        "bootstrap.servers": "pkc-n3603.us-central1.gcp.confluent.cloud:9092",
+        'security.protocol': 'SASL_SSL',
+        'sasl.mechanisms': 'PLAIN',
+        'session.timeout.ms': 50000
+    },
+    "schema_registry": {
+        "url": "https://psrc-6v15j.us-central1.gcp.confluent.cloud",
+        "basic.auth.user.info": "QRLUIVWRDWNY4EBV:8vxjbLpTccvDgxtHRpk9Z4CYfu7LeGrQrOKUNpZMXDPh+Y/Xcrkruyh/5mhO0Xzc"
+
+    }
+}
